c90a4740b620da58a99a55fc24ee358db185d6b3
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-18 19:53:05 -0400

    connected Post, User, and Comment in index.js

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const Post = require("./Post");
+const Comment = require("./Comment");
+const User = require("./User");
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+module.exports = { Post, Comment, User };
