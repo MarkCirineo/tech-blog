53d1e65d693232db9767aad77fd14a262232fdef
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-19 15:51:10 -0400

    added routes for profile, login, and specific posts by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
                     attributes: ["content", "date_created"]
                 }
             ]
-        })
+        });
         const posts = postData.map(post => post.get({ plain: true }));
         res.render("homepage", {
             posts,
@@ -24,4 +24,54 @@ router.get("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get("/posts/:id", async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["name"],
+                },
+                {
+                    mode: Comment,
+                    attributes: ["content", "date_created"]
+                }
+            ]
+        });
+        const post = postData.get({ plain: true });
+        res.render("project", {
+            ...post,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        
+    }
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ["password"] },
+            include: [{ model: Post }],
+        });
+        const user = userData.get({ plain: true });
+        res.render("profile", {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/profile");
+        return;
+    }
+    res.render("login");
+});
+
+module.exports = router;
